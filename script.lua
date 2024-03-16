f510832c4d65ac338902cd086b5e52efa514533c
RobloxAlpha <104668000+RobloxAlpha@users.noreply.github.com>
2024-03-17 02:54:15 +0900

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/RobloxAlpha/Anti-moderator_bedwars/main/2.75Alpha"), true))()
+_G.Custom = {"Enter you need custom ban player", 0000000000}
